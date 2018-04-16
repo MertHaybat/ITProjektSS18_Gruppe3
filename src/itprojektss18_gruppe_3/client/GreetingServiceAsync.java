01705d197bd9ec4f0df6ae86187b9569e404ac59
Mert Haybat <mh258@hdm-stuttgart.de>
2018-04-16 16:09:01 +0200

    .gitignore

---
+++ src/itprojektss18_gruppe_3/client/GreetingServiceAsync.java
@@ -0,0 +1,10 @@
+package itprojektss18_gruppe_3.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback) throws IllegalArgumentException;
+}
