c060cbfd43187336c421476ddfdb3581db621421
ThomasBurkhardt <tb112@hdm-stuttgart.de>
2018-05-14 16:55:09 +0200

    Erstellung der ReportKlassen AlleKontakteByTeilhaberschaftReport, AlleKontakteReport, HTMLReportWriter, KontakteMitBestimmtenEigenschaftenUndAuspraegungenReport

---
+++ src/de/hdm/itprojektss18Gruppe3/shared/report/AlleKontakteByTeilhaberschaftReport.java
@@ -0,0 +1,10 @@
+package de.hdm.itprojektss18Gruppe3.shared.report;
+
+import java.io.Serializable;
+
+public class AlleKontakteByTeilhaberschaftReport extends SimpleReport 
+	implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/de/hdm/itprojektss18Gruppe3/shared/report/KontakteMitBestimmtenEigenschaftenUndAuspraegungenReport.java
@@ -0,0 +1,10 @@
+package de.hdm.itprojektss18Gruppe3.shared.report;
+
+import java.io.Serializable;
+
+public class KontakteMitBestimmtenEigenschaftenUndAuspraegungenReport 
+	extends SimpleReport implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+}
