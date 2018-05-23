bfa93a2a01f4d676ff11bf893c6eeb8cd46397b3
WahidVanaki <vw019@hdm-stuttgart.de>
2018-05-23 17:30:13 +0200

    KontaktlisteDialogBox Callbacks implementiert

--- src/de/hdm/itprojektss18Gruppe3/client/gui/KontaktlisteDialogBox.java
+++ src/de/hdm/itprojektss18Gruppe3/client/gui/KontaktlisteDialogBox.java
@@ -7,6 +7,7 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -15,7 +16,7 @@ import de.hdm.itprojektss18Gruppe3.shared.bo.Kontaktliste;
 import de.hdm.itprojektss18Gruppe3.client.ClientsideSettings;
 
 /**
- * DialogBox zum Hinzufügen einer neuen Kontaktliste
+ * Die Klasse "KontaktlisteDialogBox" ermöglicht das Hinzufügen einer neuen Kontaktliste über eine DialogBox
  * 
  * @version 1.0 23 May 2018
  * @author wahidvanaki
@@ -23,27 +24,49 @@ import de.hdm.itprojektss18Gruppe3.client.ClientsideSettings;
  */
 public class KontaktlisteDialogBox extends DialogBox {
 	
+	/**
+	 * Instanziierung vom VerticalPanel 
+	 */
 	private VerticalPanel vPanel = new VerticalPanel();
+
 	private HorizontalPanel hPanel = new HorizontalPanel();
+	private HorizontalPanel hPanel2 = new HorizontalPanel();
 	
+	/**
+	 * Instanziierung vom Label "kontaktlisteLabel" welches als Hilfe für die TextBox dient, damit erkenntlich ist welche Angabe in die TextBox eingetragen werden soll
+	 */
 	private Label kontaktlisteLabel = new Label("Neue Kontaktliste: ");
 	
+	/**
+	 * Instanziierung der TextBox namens "tkontaktliste"
+	 */
 	private TextBox tkontaktliste = new TextBox();
 	
+	/**
+	 * Instanziierung der Buttons "speichern" und "abbrechen"
+	 * @see bspeichern, babbrechen 
+	 */
 	private Button bspeichern = new Button("speichern");
+	private Button babbrechen = new Button("abbrechen");
 	
 	private static KontaktmanagerAdministrationAsync kontaktmanagerVerwaltung = ClientsideSettings.getKontaktVerwaltung();
 	
+	/**
+	 * Non-Argument-Konstruktor
+	 */
 	public KontaktlisteDialogBox() {
 		
-		vPanel.add(kontaktlisteLabel);
-		vPanel.add(tkontaktliste);
-		vPanel.add(bspeichern);
+		hPanel.add(kontaktlisteLabel);
+		hPanel.add(tkontaktliste);
+		hPanel2.add(bspeichern);
+		hPanel2.add(babbrechen);
 		
 		vPanel.add(hPanel);
-		this.add(hPanel);
+		vPanel.add(hPanel2);
+		this.add(vPanel);
 		
 		bspeichern.addClickHandler(new insertKontaktlisteClickHandler());
+		babbrechen.addClickHandler(new closeKontaktlisteClickHandler());
 		
 	}
 	
@@ -59,6 +82,10 @@ public class KontaktlisteDialogBox extends DialogBox {
 		public void onSuccess(Kontaktliste result) {
 			// TODO Auto-generated method stub
 			hide();
+			KontaktlistView kontaktlisteBox = new KontaktlistView();
+			RootPanel.get("content").clear();
+			RootPanel.get("content").add(kontaktlisteBox);
+			
 		}
 	}
 	
@@ -69,5 +96,18 @@ public class KontaktlisteDialogBox extends DialogBox {
 			// TODO Auto-generated method stub
 			kontaktmanagerVerwaltung.createKontaktliste(tkontaktliste.getValue(), 1, new createKontaktlisteCallback());
 		}
+	}
+	
+	public class closeKontaktlisteClickHandler implements ClickHandler {
+
+		@Override
+		public void onClick(ClickEvent event) {
+			// TODO Auto-generated method stub
+			hide();
+			KontaktlistView kontaktlisteBox = new KontaktlistView();
+			RootPanel.get("content").clear();
+			RootPanel.get("content").add(kontaktlisteBox);
+		}
+		
 	}
 }
