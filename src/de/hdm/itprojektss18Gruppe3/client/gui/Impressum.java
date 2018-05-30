280187451ea7e81f39e5826492800b0a59736717
ThomasBurkhardt <tb112@hdm-stuttgart.de>
2018-05-30 20:28:49 +0200

    Erstellung von AGB und Impressum

---
+++ src/de/hdm/itprojektss18Gruppe3/client/gui/Impressum.java
@@ -0,0 +1,55 @@
+package de.hdm.itprojektss18Gruppe3.client.gui;
+
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+import de.hdm.itprojektss18Gruppe3.client.MainFrame;
+import de.hdm.itprojektss18Gruppe3.shared.bo.Nutzer;
+
+/**
+ * 
+ * HTML Seite von Impressum wird angezeigt
+ *
+ */
+public class Impressum extends MainFrame {
+
+	private VerticalPanel vpanel = new VerticalPanel();
+	private FlexTable ft1 = new FlexTable();
+	private Anchor an1 = new Anchor("Text");
+	private HTML html1 = new HTML(
+
+			"<div class='Impressum'>" + "Hochschule der Medien" + "</br>"
+					+ "<b>Wirtschaftsinformatik und Digitale Medien</b></br>" + "Nobelstrasse 10</br>"
+					+ "70569 Stuttgart</br></br>" + "Kontakt</br>Telefon: +49 711 8923 10</br>"
+					+ "<br><br>Der Studiengang Wirtschaftsinformatik und digitale "
+					+ "Medien<br>wird rechtlich vertreten durch die Hochschule der Med"
+					+ "ien Stuttgart. <br> <br><A HREF=\"https://www.hdm-stuttgart.de/"
+					+ "impressum\"TARGET=\"_blank\">Impressum der Hochschule</A>" + "</div>");
+
+	
+	/**
+	 * 
+	 * @param nutzer
+	 */
+//	public Impressum(final Nutzer nutzer) {
+//		ft1.setWidget(0, 0, an1);
+//		ft1.setWidget(0, 1, html1);
+//
+//		vpanel.add(ft1);
+//		this.add(vpanel);
+//
+//	}
+
+
+	@Override
+	protected void run() {
+		ft1.setWidget(0, 0, an1);
+		ft1.setWidget(0, 0, html1);
+		
+		vpanel.add(ft1);
+		this.add(vpanel);
+		
+	}
+}
