9deccf2611f2442f7a1d774641d4faedd24e4e46
Giuseppe Galati <gg025@hdm-stuttgart.de>
2018-06-03 17:18:49 +0200

    EigenschaftsAuspraegungWrapper angelegt

---
+++ src/de/hdm/itprojektss18Gruppe3/client/EigenschaftsAuspraegungWrapper.java
@@ -0,0 +1,77 @@
+package de.hdm.itprojektss18Gruppe3.client;
+
+import java.io.Serializable;
+
+import de.hdm.itprojektss18Gruppe3.shared.bo.Eigenschaft;
+import de.hdm.itprojektss18Gruppe3.shared.bo.Eigenschaftsauspraegung;
+
+public class EigenschaftsAuspraegungWrapper  {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Eigenschaft eigenschaft = new Eigenschaft();
+	private Eigenschaftsauspraegung auspraegung = new Eigenschaftsauspraegung();
+	
+	
+	public EigenschaftsAuspraegungWrapper(){
+		
+	}
+	
+
+	
+	public EigenschaftsAuspraegungWrapper(Eigenschaft eigenschaft, Eigenschaftsauspraegung auspraegung){
+		this.eigenschaft = eigenschaft;
+		this.auspraegung = auspraegung;
+	}
+
+	public String getBezeichnungEigenschaftValue(){
+		return this.eigenschaft.getBezeichnung();
+	}
+	public void setBezeichnungEigenschaftValue(String bezeichnung){
+		this.eigenschaft.setBezeichnung(bezeichnung);
+	}
+	public int getEigenschaftIdValue(){
+		return this.eigenschaft.getId();
+	}
+	public void setEigenschaftIdValue(int eigenschaftID){
+		this.eigenschaft.setId(eigenschaftID);
+	}
+	
+	public int getPersonIdEigenschaftsauspraegungValue(){
+		return this.auspraegung.getPersonID();
+	}
+	public void setPersonIdEigenschaftsauspraegungValue(int personID){
+		this.auspraegung.setPersonID(personID);
+	}
+	public String getWertEigenschaftsauspraegungValue(){
+		return this.auspraegung.getWert();
+	}
+	public void setWertEigenschaftsauspraegungValue(String wert){
+		this.auspraegung.setWert(wert);
+	}
+	public int getIDEigenschaftsauspraegungValue(){
+		return this.auspraegung.getId();
+	}
+	public void setIDEigenschaftsauspraegungValue(int eigenschaftsauspraegungID){
+		this.auspraegung.setId(eigenschaftsauspraegungID);
+	}
+	public int getEigenschaftFKIdValue(){
+		return this.auspraegung.getEigenschaftID();
+	}
+	public void setEigenschaftFKIdValue(int eigenschaftID){
+		this.auspraegung.setEigenschaftID(eigenschaftID);
+	}
+	
+	public int getStatusValue(){
+		return this.auspraegung.getStatus();
+	}
+	
+	public void setStatusValue(int status){
+		this.auspraegung.setStatus(status);
+	}
+	
+}
+
