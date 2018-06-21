31bd60ba0c68ce996abf849ced8868a7446b4f96
ersinbarut <eb046@hdm-stuttgart.de>
2018-06-21 17:36:01 +0200

    CellTable erstellt um Teilhaberschaften zu verwalten

---
+++ src/de/hdm/itprojektss18Gruppe3/client/gui/CellTableTeilhaberschaftKontaktliste.java
@@ -0,0 +1,61 @@
+package de.hdm.itprojektss18Gruppe3.client.gui;
+
+import com.google.gwt.view.client.CellPreviewEvent.Handler;
+import com.google.gwt.cell.client.Cell;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.view.client.DefaultSelectionEventManager;
+import com.google.gwt.view.client.MultiSelectionModel;
+
+import de.hdm.itprojektss18Gruppe3.client.NutzerTeilhaberschaftKontaktWrapper;
+import de.hdm.itprojektss18Gruppe3.client.NutzerTeilhaberschaftKontaktlisteWrapper;
+
+public class CellTableTeilhaberschaftKontaktliste extends CellTable <NutzerTeilhaberschaftKontaktlisteWrapper>{
+	
+	private final MultiSelectionModel<NutzerTeilhaberschaftKontaktlisteWrapper> ssmAuspraegung =
+			new MultiSelectionModel<NutzerTeilhaberschaftKontaktlisteWrapper>();
+	private final Handler <NutzerTeilhaberschaftKontaktlisteWrapper> selectionEventManager = 
+			DefaultSelectionEventManager.createCheckboxManager();
+
+	public CellTableTeilhaberschaftKontaktliste (){
+		run();
+	}
+
+	private void run() {
+		// TODO Auto-generated method stub
+		this.setSelectionModel(ssmAuspraegung, selectionEventManager);
+	}
+	
+	public class TeilhaberschaftKontaktliste extends Column <NutzerTeilhaberschaftKontaktlisteWrapper, String>{
+		
+		public TeilhaberschaftKontaktliste(Cell<String>cell){
+			super(cell);
+		}
+		
+		public String getValue (NutzerTeilhaberschaftKontaktlisteWrapper object){
+		return object.getKontaktliste().getBezeichnung();
+		}
+	}
+	
+	public class TeilhaberschaftNutzer extends Column <NutzerTeilhaberschaftKontaktlisteWrapper, String>{
+		
+		public TeilhaberschaftNutzer(Cell<String>cell){
+			super(cell);
+		}
+		public String getValue(NutzerTeilhaberschaftKontaktlisteWrapper object){
+			return object.getNutzer().getMail();
+		}
+	}
+	
+	public class CheckColumn extends Column<NutzerTeilhaberschaftKontaktlisteWrapper, Boolean>{
+
+		public CheckColumn(Cell<Boolean> cell) {
+			super(cell);
+		}
+		@Override
+		public Boolean getValue(NutzerTeilhaberschaftKontaktlisteWrapper object) {
+			return ssmAuspraegung.isSelected(object);
+		}
+		
+	}
+}
